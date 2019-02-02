e90066fed2abae1c019fc798f4c0248a33b48447
heyzhuyue <1301529626@qq.com>
2019-02-02 15:40:40 +0800

    - 新增进制工具类

---
+++ util_lib/src/main/java/com/zy/util/HexUtils.java
@@ -0,0 +1,357 @@
+package com.zy.util;
+
+import android.text.TextUtils;
+
+import java.nio.charset.Charset;
+
+/**
+ * 进制转换工具类
+ */
+public class HexUtils {
+
+    public HexUtils() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(int2ByteArray(1));
+
+        //byte类型所占的字节数求法
+        System.out.println("byte的二进制位数为：" + Byte.SIZE);
+        System.out.println("byte所占的字节数为：" + Byte.SIZE / 8);     //一个字节占8个二进制位
+        //short类型所占的字节数求法
+        System.out.println("short的二进制位数为：" + Short.SIZE);
+        System.out.println("short所占的字节数为：" + Short.SIZE / 8);
+        //int类型所占的字节数求法
+        System.out.println("int的二进制位数为：" + Integer.SIZE);
+        System.out.println("int所占的字节数为：" + Integer.SIZE / 8);
+        //long类型所占的字节数求法
+        System.out.println("long的二进制位数为：" + Long.SIZE);
+        System.out.println("long所占的字节数为：" + Long.SIZE / 8);
+        //float类型所占的字节数求法
+        System.out.println("float的二进制位数为：" + Float.SIZE);
+        System.out.println("float所占的字节数为：" + Float.SIZE / 8);
+        //double类型所占的字节数求法
+        System.out.println("double的二进制位数为：" + Double.SIZE);
+        System.out.println("double所占的字节数为：" + Double.SIZE / 8);
+        //char类型所占的字节数求法
+        System.out.println("char的二进制位数为：" + Character.SIZE);
+        System.out.println("char所占的字节数为：" + Character.SIZE / 8);
+    }
+
+    /**
+     * 用于建立十六进制字符的输出的小写字符数组
+     */
+    private static final char[] DIGITS_LOWER = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    /**
+     * 用于建立十六进制字符的输出的大写字符数组
+     */
+    private static final char[] DIGITS_UPPER = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+    public enum HexType {
+        /**
+         * HEX:十六进制
+         * OCTAL:八进制
+         * BINARY:二进制
+         */
+        HEX, OCTAL, BINARY
+    }
+
+    /**
+     * 将Int类型值转换为进制字符串
+     *
+     * @param value   值
+     * @param hexType 进制类型
+     * @return
+     */
+    public static String int2HexString(int value, HexType hexType) {
+        switch (hexType) {
+            case HEX:
+                return Integer.toHexString(value);
+            case OCTAL:
+                return Integer.toOctalString(value);
+            case BINARY:
+                return Integer.toBinaryString(value);
+            default:
+                return Integer.toHexString(value);
+        }
+    }
+
+    /**
+     * 将字节数组转换为十六进制字符数组
+     *
+     * @param data byte[]
+     * @return 十六进制char[]
+     */
+    public static char[] encodeHex(byte[] data) {
+        return encodeHex(data, true);
+    }
+
+    /**
+     * 将字符串转换为十六进制字符数组
+     *
+     * @param str     字符串
+     * @param charset 编码
+     * @return 十六进制char[]
+     */
+    public static char[] encodeHex(String str, Charset charset) {
+        return encodeHex(str.getBytes(charset), true);
+    }
+
+    /**
+     * 将字节数组转换为十六进制字符数组
+     *
+     * @param data        byte[]
+     * @param toLowerCase <code>true</code> 传换成小写格式 ， <code>false</code> 传换成大写格式
+     * @return 十六进制char[]
+     */
+    public static char[] encodeHex(byte[] data, boolean toLowerCase) {
+        return encodeHex(data, toLowerCase ? DIGITS_LOWER : DIGITS_UPPER);
+    }
+
+    /**
+     * 将字节数组转换为十六进制字符串
+     *
+     * @param data byte[]
+     * @return 十六进制String
+     */
+    public static String encodeHexStr(byte[] data) {
+        return encodeHexStr(data, true);
+    }
+
+    /**
+     * 将字节数组转换为十六进制字符串
+     *
+     * @param data        byte[]
+     * @param toLowerCase <code>true</code> 传换成小写格式 ， <code>false</code> 传换成大写格式
+     * @return 十六进制String
+     */
+    public static String encodeHexStr(byte[] data, boolean toLowerCase) {
+        return encodeHexStr(data, toLowerCase ? DIGITS_LOWER : DIGITS_UPPER);
+    }
+
+    /**
+     * 将十六进制字符数组转换为字符串
+     *
+     * @param hexStr  十六进制String
+     * @param charset 编码
+     * @return 字符串
+     */
+    public static String decodeHexStr(String hexStr, Charset charset) {
+        if (TextUtils.isEmpty(hexStr)) {
+            return hexStr;
+        }
+        return decodeHexStr(hexStr.toCharArray(), charset);
+    }
+
+    /**
+     * 将十六进制字符数组转换为字符串
+     *
+     * @param hexData 十六进制char[]
+     * @param charset 编码
+     * @return 字符串
+     */
+    public static String decodeHexStr(char[] hexData, Charset charset) {
+        return new String(decodeHex(hexData), charset);
+    }
+
+    /**
+     * 将字节数组转换为十六进制字符串
+     *
+     * @param data     byte[]
+     * @param toDigits 用于控制输出的char[]
+     * @return 十六进制String
+     */
+    private static String encodeHexStr(byte[] data, char[] toDigits) {
+        return new String(encodeHex(data, toDigits));
+    }
+
+    /**
+     * 将字节数组转换为十六进制字符数组
+     *
+     * @param data     byte[]
+     * @param toDigits 用于控制输出的char[]
+     * @return 十六进制char[]
+     */
+    private static char[] encodeHex(byte[] data, char[] toDigits) {
+        int l = data.length;
+        char[] out = new char[l << 1];
+        // two characters form the hex value.
+        for (int i = 0, j = 0; i < l; i++) {
+            out[j++] = toDigits[(0xF0 & data[i]) >>> 4];
+            out[j++] = toDigits[0x0F & data[i]];
+        }
+        return out;
+    }
+
+    /**
+     * 将十六进制字符数组转换为字节数组
+     *
+     * @param hexData 十六进制char[]
+     * @return byte[]
+     * @throws RuntimeException 如果源十六进制字符数组是一个奇怪的长度，将抛出运行时异常
+     */
+    private static byte[] decodeHex(char[] hexData) {
+
+        int len = hexData.length;
+
+        if ((len & 0x01) != 0) {
+            throw new RuntimeException("Odd number of characters.");
+        }
+
+        byte[] out = new byte[len >> 1];
+
+        // two characters form the hex value.
+        for (int i = 0, j = 0; j < len; i++) {
+            int f = toDigit(hexData[j], j) << 4;
+            j++;
+            f = f | toDigit(hexData[j], j);
+            j++;
+            out[i] = (byte) (f & 0xFF);
+        }
+
+        return out;
+    }
+
+    /**
+     * 将十六进制字符转换成一个整数
+     *
+     * @param ch    十六进制char
+     * @param index 十六进制字符在字符数组中的位置
+     * @return 一个整数
+     * @throws RuntimeException 当ch不是一个合法的十六进制字符时，抛出运行时异常
+     */
+    private static int toDigit(char ch, int index) {
+        int digit = Character.digit(ch, 16);
+        if (digit == -1) {
+            throw new RuntimeException("Illegal hexadecimal character " + ch + " at index " + index);
+        }
+        return digit;
+    }
+
+    /**
+     * 2进制转16进制
+     *
+     * @param bString 2进制字符串
+     * @return
+     */
+    public static String binary2Hex(String bString) {
+        if (bString == null || bString.equals("") || bString.length() % 8 != 0)
+            return null;
+        StringBuffer tmp = new StringBuffer();
+        int iTmp = 0;
+        for (int i = 0; i < bString.length(); i += 4) {
+            iTmp = 0;
+            for (int j = 0; j < 4; j++) {
+                iTmp += Integer.parseInt(bString.substring(i + j, i + j + 1)) << (4 - j - 1);
+            }
+            tmp.append(Integer.toHexString(iTmp));
+        }
+        return tmp.toString();
+    }
+
+    /**
+     * 16进制转2进制
+     *
+     * @param hexString
+     * @return
+     */
+    public static String hex2Binary(String hexString) {
+        if (hexString == null || hexString.length() % 2 != 0)
+            return null;
+        String bString = "", tmp;
+        for (int i = 0; i < hexString.length(); i++) {
+            tmp = "0000" + Integer.toBinaryString(Integer.parseInt(hexString.substring(i, i + 1), 16));
+            bString += tmp.substring(tmp.length() - 4);
+        }
+        return bString;
+    }
+
+    /**
+     * 将二进制转换成16进制
+     *
+     * @param buf
+     * @return
+     */
+    public static String binary2Hex(byte buf[]) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < buf.length; i++) {
+            String hex = Integer.toHexString(buf[i] & 0xFF);
+            if (hex.length() == 1) {
+                hex = '0' + hex;
+            }
+            sb.append(hex.toUpperCase());
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 将16进制转换为二进制
+     *
+     * @param hexStr
+     * @return
+     */
+    public static byte[] hex2Byte(String hexStr) {
+        if (hexStr.length() < 1)
+            return null;
+        byte[] result = new byte[hexStr.length() / 2];
+        for (int i = 0; i < hexStr.length() / 2; i++) {
+            int high = Integer.parseInt(hexStr.substring(i * 2, i * 2 + 1), 16);
+            int low = Integer.parseInt(hexStr.substring(i * 2 + 1, i * 2 + 2), 16);
+            result[i] = (byte) (high * 16 + low);
+        }
+        return result;
+    }
+
+    /**
+     * int转byte[]
+     * 该方法将一个int类型的数据转换为byte[]形式，因为int为32bit，而byte为8bit所以在进行类型转换时，知会获取低8位，
+     * 丢弃高24位。通过位移的方式，将32bit的数据转换成4个8bit的数据。注意 &0xff，在这当中，&0xff简单理解为一把剪刀，
+     * 将想要获取的8位数据截取出来。
+     *
+     * @param i 一个int数字
+     * @return byte[]
+     */
+
+
+    public static byte[] int2ByteArray(int i) {
+        byte[] result = new byte[4];
+        result[0] = (byte) ((i >> 24) & 0xFF);
+        result[1] = (byte) ((i >> 16) & 0xFF);
+        result[2] = (byte) ((i >> 8) & 0xFF);
+        result[3] = (byte) (i & 0xFF);
+        return result;
+    }
+
+    /**
+     * int转byte[]
+     *
+     * @param value 数值
+     * @return byte[]
+     */
+    public static byte[] int2Bytes(int value) {
+        byte[] bytes = new byte[4];
+        bytes[3] = (byte) ((byte) value >> 24);
+        bytes[2] = (byte) ((byte) value >> 16);
+        bytes[1] = (byte) ((byte) value >> 8);
+        bytes[0] = (byte) value;
+
+        return bytes;
+    }
+
+    /**
+     * byte[]转int
+     * 利用int2ByteArray方法，将一个int转为byte[]，但在解析时，需要将数据还原。同样使用移位的方式，将适当的位数进行还原，
+     * 0xFF为16进制的数据，所以在其后每加上一位，就相当于二进制加上4位。同时，使用|=号拼接数据，将其还原成最终的int数据
+     *
+     * @param bytes byte类型数组
+     * @return int数字
+     */
+    public static int bytes2Int(byte[] bytes) {
+        int int1 = bytes[0] & 0xff;
+        int int2 = (bytes[1] & 0xff) << 8;
+        int int3 = (bytes[2] & 0xff) << 16;
+        int int4 = (bytes[3] & 0xff) << 24;
+        return int1 | int2 | int3 | int4;
+    }
+}
